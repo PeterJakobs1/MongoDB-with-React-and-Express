f84ba00492ea41b0c9a64db90e72baabc27336ba
PeterJakobs1 <peter.bj@live.no>
2022-03-06 15:51:44 +0100

    "complete" server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import express from 'express';
 
 const app = express();
 
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server started on http://localhost:${server.address().port}`);
 });
+
+// heroku login   git remote    heroku git:remote -a lecture-7

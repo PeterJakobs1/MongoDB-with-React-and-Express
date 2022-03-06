d41bc4cc2d8826fc514bb83aff085459256b342c
PeterJakobs1 <peter.bj@live.no>
2022-03-06 16:49:27 +0100

    React app for heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static("../client/dist/"));
+
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`server started on http://localhost:${server.address().port}`);
